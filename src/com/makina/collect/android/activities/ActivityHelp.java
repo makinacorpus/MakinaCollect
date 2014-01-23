18896fa19de2a56c55d1926f6a83de6f9063ee2b
Alaeddine Ouertani <ouertani.alaeddine@gmail.com>
2014-01-23 01:47:25 +0100

    fixed #61

--- src/com/makina/collect/android/activities/ActivityHelp.java
+++ src/com/makina/collect/android/activities/ActivityHelp.java
@@ -1,10 +1,13 @@
 package com.makina.collect.android.activities;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.makina.collect.android.R;
 import com.makina.collect.android.dialog.DialogHelp;
 import com.makina.collect.android.utilities.Finish;
-
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 public class ActivityHelp extends FragmentActivity {
 	
@@ -14,11 +17,16 @@ public class ActivityHelp extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
 		
+		Window window = getWindow();
+	    WindowManager.LayoutParams wlp = window.getAttributes();
+	    wlp.gravity = Gravity.CENTER_VERTICAL;
+	    wlp.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+	    wlp.dimAmount = (float) 0.6;
+	    window.setAttributes(wlp);
 		Finish.activityHelp=this;
 		DialogHelp dialog = new DialogHelp(getIntent().getExtras().getInt("position"));
 		dialog.show(getSupportFragmentManager(), "");
 		dialog.setTargetFragment(dialog.getTargetFragment(),DIALOG_RESULT);
-		
 	}
 	
 }
