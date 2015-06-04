febcb574c7aae3746c24f8db1dad35af9bfeb5ac
S. Grimault <sebastien.grimault@makina-corpus.com>
2015-06-04 08:48:38 +0200

    Theme management

--- app/src/main/java/com/makina/collect/android/activities/AbstractBaseActivity.java
+++ app/src/main/java/com/makina/collect/android/activities/AbstractBaseActivity.java
@@ -1,9 +1,12 @@
 package com.makina.collect.android.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import com.makina.collect.android.R;
+import com.makina.collect.android.utilities.ThemeUtils;
 
 import ly.count.android.api.Countly;
 
@@ -12,29 +15,53 @@ import ly.count.android.api.Countly;
  *
  * @author <a href="mailto:sebastien.grimault@makina-corpus.com">S. Grimault</a>
  */
-public abstract class AbstractBaseActivity extends ActionBarActivity {
+public abstract class AbstractBaseActivity
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ThemeUtils.apply(this,
+                         applyTheme());
+
         super.onCreate(savedInstanceState);
 
-        Countly.sharedInstance().init(
-                this,
-                getString(R.string.__config_analytics_server_url),
-                getString(R.string.__config_analytics_app_key));
+        Countly.sharedInstance()
+               .init(this,
+                     getString(R.string.__config_analytics_server_url),
+                     getString(R.string.__config_analytics_app_key));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Countly.sharedInstance().onStart();
+        Countly.sharedInstance()
+               .onStart();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        Countly.sharedInstance().onStop();
+        Countly.sharedInstance()
+               .onStop();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        ThemeUtils.apply(this,
+                         applyTheme());
+    }
+
+    /**
+     * Override this method to apply a theme variant for this {@code Activity}.
+     *
+     * @return the theme variant to apply (may be {@code null})
+     */
+    @Nullable
+    protected ThemeUtils.AppThemeVariant applyTheme() {
+        return null;
     }
 }
